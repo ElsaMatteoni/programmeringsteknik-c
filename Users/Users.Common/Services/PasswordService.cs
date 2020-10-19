69306f632194202826bf9ffd883d87b9046643b4
Elsa Matteoni <elsa.matteoni@hotmail.com>
2020-10-19 14:56:43 +0200

    Changes

--- Users/Users.Common/Services/PasswordService.cs
+++ Users/Users.Common/Services/PasswordService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using Users.Common.Models;
 
 namespace Users.Common.Services
@@ -7,16 +9,53 @@ namespace Users.Common.Services
     {
         private const string _letters = "abcdefghijklmnopqrstuvwxyz";
         private const string _numbers = "1234567890";
-        private const string _special = "$%#@!*?;:^&";
+        private const string _special = Constants.Passwords.SpecialCharacters;
 
+       
+
+        
         public string GeneratePassword(uint length, uint capitalLetters = 1, uint numbers = 1, uint specialChars = 1)
+        {
+            if (capitalLetters + numbers + specialChars > length)
+                throw new ArgumentOutOfRangeException("Capital letters, numbers and special characters exceed allowed password length");
+
+            var buffer = new char[length];
+
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                var currentCharacter = GetRandomCharachter(_letters);
+
+                if (i < capitalLetters)
+                {
+                    currentCharacter = char.ToUpper(currentCharacter);
+                }
+                
+                else if (i >= length - specialChars)
+                {
+                    currentCharacter = GetRandomCharachter(_special);
+                }
+                else if (i >= length - numbers - specialChars)
+                {
+                    currentCharacter = GetRandomCharachter(_numbers);
+                }
+
+
+                buffer[i] = currentCharacter;
+            }
+
+            return new string(buffer);
+        }
+
+        public IServiceResponse ValidatePassword(string password, uint length, uint capitalLetters = 1, uint numbers = 1, uint specialChars = 1)
         {
             throw new NotImplementedException();
         }
-
-        public IServiceResponse ValidatePassword(uint length, uint capitalLetters = 1, uint numbers = 1, uint specialChars = 1)
+        private char GetRandomCharachter(string input)
         {
-            throw new NotImplementedException();
+            
+            var position = new Random().Next(0, input.Length);
+            return input[position];
+            
         }
     }
 }
---
+++ Users/UsersTest/UsersTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UsersTest
+{
+    [TestClass]
+    public class UsersTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
